0aeb57cab5f67e6ff959544423a4eeeac8de1746
Baptiste Bronsin <Baptiste.BRONSIN@consulting-for.accor.com>
2025-02-07 19:51:56 +0100

    add link to characters list page

--- src/feature/web-site-url.feature.tsx
+++ src/feature/web-site-url.feature.tsx
@@ -5,7 +5,10 @@ const WebSiteUrl: React.FC = () => {
 
     return (
         <div className="hidden sm:flex flex-row justify-between items-center">
-            <p>Ce site web est accessible depuis le lien <a href={domainName} rel="Lien vers le site de présentation de l'activité" className="underline text-[#4169E1]">{ domainName }</a></p>
+            <div className='h-full flex flex-col justify-center gap-6'>
+                <p>Ce site web est accessible depuis le lien <a href={domainName} rel="Lien vers le site de présentation de l'activité" className="underline text-[#4169E1]">{ domainName }</a></p>
+                <p>Vous ne savez pas quel personnage prendre ? <a href='/characters' rel='Lien vers la page avec tous les personnages de Rick et Morty' className='underline text-[#4169E1]'>Liste des personnages</a></p>
+            </div>
             <QRCode value={ domainName } logoImage='/images/polytech.png' fgColor='#009de0' removeQrCodeBehindLogo={true} qrStyle='dots' style={{ borderRadius: '8px' }} />
         </div>
     )
