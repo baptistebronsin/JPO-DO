f8249e4f91cfaaf9610ff0a90b638ca81150a76f
Baptiste Bronsin <Baptiste.BRONSIN@consulting-for.accor.com>
2025-02-07 18:44:41 +0100

    add a blank attribute

--- src/feature/footer.feature.tsx
+++ src/feature/footer.feature.tsx
@@ -6,7 +6,7 @@ const Footer: React.FC = () => {
     return (
         <footer className="flex flex-row gap-2 items-center">
             <Gitlab color="#FC6D26" />
-            <p>Dépôt Gitlab : <a href={ gitlabRepo } rel="Lien du dépôt Gitlab utilisé" className="underline text-[#4169E1]">{ gitlabRepo }</a></p>
+            <p>Dépôt Gitlab : <a href={ gitlabRepo } rel="Lien du dépôt Gitlab utilisé" target="_blank" className="underline text-[#4169E1]">{ gitlabRepo }</a></p>
         </footer>
     )
 }
