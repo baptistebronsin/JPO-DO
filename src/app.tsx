bdd5c64dba088eba9696029501d6d49d67f38bfa
Baptiste Bronsin <Baptiste.BRONSIN@consulting-for.accor.com>
2025-02-07 18:38:37 +0100

    update main title in bold

--- src/app.tsx
+++ src/app.tsx
@@ -5,7 +5,7 @@ const App: React.FC = () => {
     return (
         <template className='flex justify-center py-6 px-4'>
             <div className='max-w-7xl w-full flex flex-col gap-12'>
-                <h1 className="text-lg">Bienvenue sur la page de la Journée Portes Ouvertes de la formation d’ingénieur DevOps 👋🏼</h1>
+                <h1 className="text-lg font-bold">Bienvenue sur la page de la Journée Portes Ouvertes de la formation d’ingénieur DevOps 👋🏼</h1>
                 <WebSiteUrl />
                 <Activity />
                 <Contacts />
