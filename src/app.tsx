130e4cddc88b8108f9885dee8201352e6cd8b6ad
Baptiste Bronsin <Baptiste.BRONSIN@consulting-for.accor.com>
2025-02-07 15:19:30 +0100

    update domain link

--- src/app.tsx
+++ src/app.tsx
@@ -8,7 +8,7 @@ const App: React.FC = () => {
         <template className='flex justify-center py-6 px-4'>
             <div className='max-w-7xl w-full flex flex-col gap-8'>
                 <h1 className="text-lg">Bienvenue sur la page de la Journée Portes Ouvertes de la formation d’ingénieur DevOps 👋🏼</h1>
-                <p>Ce site web est accessible depuis le lien <span className="underline text-[#4169E1]">{ domainName }</span></p>
+                <p>Ce site web est accessible depuis le lien <a href={domainName} rel="Lien vers le site de présentation de l'activité" className="underline text-[#4169E1]">{ domainName }</a></p>
                 <Activity />
                 <Contacts />
             </div>
