679b383899ba1281d5452c627d5f6dd157528fc2
Proxyfil <piloupilou200@gmail.com>
2025-02-07 18:51:01 +0100

    fix : Centered footer

--- src/feature/footer.feature.tsx
+++ src/feature/footer.feature.tsx
@@ -4,7 +4,7 @@ const Footer: React.FC = () => {
     const gitlabRepo: string = "https://gitlab.com/baptiste.bronsin/jpo-do";
 
     return (
-        <footer className="flex flex-row gap-2 items-center">
+        <footer className="flex flex-row gap-2 items-center justify-center">
             <Gitlab color="#FC6D26" />
             <p>Dépôt Gitlab : <a href={ gitlabRepo } rel="Lien du dépôt Gitlab utilisé" target="_blank" className="underline text-[#4169E1]">{ gitlabRepo }</a></p>
         </footer>
