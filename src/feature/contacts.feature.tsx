8544ddf936fe760480e4bafb7c5d981824a8d63d
Proxyfil <piloupilou200@gmail.com>
2025-02-07 18:51:25 +0100

    feat : Added 2 contacts

--- src/feature/contacts.feature.tsx
+++ src/feature/contacts.feature.tsx
@@ -6,9 +6,11 @@ const Contacts: React.FC = () => {
         <div className="flex flex-col gap-4">
             <h2 className="text-lg">Restons en contact !</h2>
             <p>Si vous avez d’autres questions sur la formation, l’alternance, la vie étudiante/apprentie ou juste envie de boire une bière alors voici nos contacts :</p>
-            <div className="flex flex-col gap-4">
+            <div className="grid grid-cols-1 sm:grid-cols-1 lg:grid-cols-2 gap-x-4 gap-y-6">
                 <Contact fullName="Baptiste BRONSIN" catchPhrase="Je suis en DO4 et j’aime les ambrées ! 🍻" image="/images/people/baptiste_bronsin.jpg" linkedIn="https://www.linkedin.com/in/baptiste-bronsin" />
                 <Contact fullName="Sarah THEOULLE" catchPhrase="Sheep huntress and DO4" image="/images/people/sarah_theoulle.png" linkedIn="https://www.linkedin.com/in/stheoulle/" />
+                <Contact fullName="Hugo DU PELOUX" catchPhrase="Average Coding Enjoyer" image="/images/people/hugo_du_peloux.png" linkedIn="https://www.linkedin.com/in/hugo-du-peloux/" />
+                <Contact fullName="Pierre-Louis LECLERC" catchPhrase="DO3 bienveillant et master mouton ! 🐑" image="/images/people/pierre-louis_leclerc.png" linkedIn="https://www.linkedin.com/in/pierre-louis-leclerc/" />
             </div>
         </div>
     )
