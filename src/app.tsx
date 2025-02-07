0cee7b8468677b358c9b1930490da67a6ad445a3
Baptiste Bronsin <Baptiste.BRONSIN@consulting-for.accor.com>
2025-02-07 18:29:09 +0100

    add footer

--- src/app.tsx
+++ src/app.tsx
@@ -1,16 +1,15 @@
-import Activity from "./feature/activity.feature";
-import Contacts from "./feature/contacts.feature";
-import WebSiteUrl from "./feature/web-site-url.feature";
+import { Activity, Contacts, Footer, WebSiteUrl } from "./feature";
 
 const App: React.FC = () => {
 
     return (
         <template className='flex justify-center py-6 px-4'>
-            <div className='max-w-7xl w-full flex flex-col gap-8'>
+            <div className='max-w-7xl w-full flex flex-col gap-12'>
                 <h1 className="text-lg">Bienvenue sur la page de la Journée Portes Ouvertes de la formation d’ingénieur DevOps 👋🏼</h1>
                 <WebSiteUrl />
                 <Activity />
                 <Contacts />
+                <Footer />
             </div>
         </template>
     );
---
+++ src/feature/footer.feature.tsx
@@ -0,0 +1,14 @@
+import { Gitlab } from "lucide-react";
+
+const Footer: React.FC = () => {
+    const gitlabRepo: string = "https://gitlab.com/baptiste.bronsin/jpo-do";
+
+    return (
+        <footer className="flex flex-row gap-2 items-center">
+            <Gitlab color="#FC6D26" />
+            <p>Dépôt Gitlab : <a href={ gitlabRepo } rel="Lien du dépôt Gitlab utilisé" className="underline text-[#4169E1]">{ gitlabRepo }</a></p>
+        </footer>
+    )
+}
+
+export default Footer;
