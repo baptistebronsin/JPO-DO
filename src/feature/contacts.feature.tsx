9764d39c0471d46f88b8eb3a899842af0c0d42c1
Sarah <sarah.theoulle@etu.umontpellier.fr>
2025-02-07 15:14:12 +0100

    fix(image)

--- src/feature/contacts.feature.tsx
+++ src/feature/contacts.feature.tsx
@@ -8,7 +8,7 @@ const Contacts: React.FC = () => {
             <p>Si vous avez d’autres questions sur la formation, l’alternance, la vie étudiante/apprentie ou juste envie de boire une bière alors voici nos contacts :</p>
             <div className="flex flex-col gap-4">
             <Contact fullName="Baptiste BRONSIN" catchPhrase="Je suis en DO4 et j’aime les ambrées ! 🍻" image="/images/baptiste_bronsin.jpg" linkedIn="https://www.linkedin.com/in/baptiste-bronsin" />
-            <Contact fullName="Sarah THEOULLE" catchPhrase="Sheep huntress and DO4" image="/images/sarah_theoulle.jpg" linkedIn="https://www.linkedin.com/in/stheoulle/" />
+            <Contact fullName="Sarah THEOULLE" catchPhrase="Sheep huntress and DO4" image="/images/sarah_theoulle.png" linkedIn="https://www.linkedin.com/in/stheoulle/" />
             </div>
         </div>
     )
