ef4e53893cad530979e6b1fab2494155590364ca
Baptiste Bronsin <Baptiste.BRONSIN@consulting-for.accor.com>
2025-02-07 17:19:58 +0100

    remove logo padding

--- src/feature/web-site-url.feature.tsx
+++ src/feature/web-site-url.feature.tsx
@@ -6,7 +6,7 @@ const WebSiteUrl: React.FC = () => {
     return (
         <div className="hidden sm:flex flex-row justify-between items-center">
             <p>Ce site web est accessible depuis le lien <a href={domainName} rel="Lien vers le site de présentation de l'activité" className="underline text-[#4169E1]">{ domainName }</a></p>
-            <QRCode value={ domainName } logoImage='/images/polytech.png' fgColor='#009de0' logoPadding={8} removeQrCodeBehindLogo={true} qrStyle='dots' style={{ borderRadius: '8px' }} />
+            <QRCode value={ domainName } logoImage='/images/polytech.png' fgColor='#009de0' removeQrCodeBehindLogo={true} qrStyle='dots' style={{ borderRadius: '8px' }} />
         </div>
     )
 }
