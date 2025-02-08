d7b520894ef17f44dad0d9dfbebcb3fb4f0da9c9
Baptiste Bronsin <baptiste.bronsin@outlook.com>
2025-02-08 09:03:16 +0100

    update card component to display Vincent B. instead of Sacha

--- src/feature/cards.feature.tsx
+++ src/feature/cards.feature.tsx
@@ -1,11 +1,11 @@
-//import Card from "./card.feature";
+import Card from "./card.feature";
 
 const Cards: React.FC = () => {
 
     return (
         // <div className="flex flex-wrap justify-between gap-y-4">
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-x-8 gap-y-4">
-            {/* <Card firstName="Sacha" characterId={1} /> */}
+            <Card firstName="Vincent B." characterId={8} />
         </div>
     )
 }
