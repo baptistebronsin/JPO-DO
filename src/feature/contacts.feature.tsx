70016ebbeebef598e7d68404298dc46a96db0cfd
Baptiste Bronsin <baptiste.bronsin@outlook.com>
2025-02-08 08:55:35 +0100

    update catchphrase for Hugo DU PELOUX in contacts feature

--- src/feature/contacts.feature.tsx
+++ src/feature/contacts.feature.tsx
@@ -9,7 +9,7 @@ const Contacts: React.FC = () => {
             <div className="grid grid-cols-1 sm:grid-cols-1 lg:grid-cols-2 gap-x-4 gap-y-6">
                 <Contact fullName="Baptiste BRONSIN" catchPhrase="Je suis en DO4 et j’aime les ambrées ! 🍻" image="/images/people/baptiste_bronsin.jpg" linkedIn="https://www.linkedin.com/in/baptiste-bronsin" />
                 <Contact fullName="Sarah THEOULLE" catchPhrase="Sheep huntress and DO4" image="/images/people/sarah_theoulle.png" linkedIn="https://www.linkedin.com/in/stheoulle/" />
-                <Contact fullName="Hugo DU PELOUX" catchPhrase="Average Coding Enjoyer" image="/images/people/hugo_du_peloux.png" linkedIn="https://www.linkedin.com/in/hugo-du-peloux/" />
+                <Contact fullName="Hugo DU PELOUX" catchPhrase="DO3 le jour, 3DO la nuit" image="/images/people/hugo_du_peloux.png" linkedIn="https://www.linkedin.com/in/hugo-du-peloux/" />
                 <Contact fullName="Pierre-Louis LECLERC" catchPhrase="DO3 bienveillant et master mouton ! 🐑" image="/images/people/pierre-louis_leclerc.png" linkedIn="https://www.linkedin.com/in/pierre-louis-leclerc/" />
             </div>
         </div>
