b57b4e055b43a85ec1048b6b596b117424609b7a
Baptiste Bronsin <baptiste.bronsin@outlook.com>
2025-02-08 09:30:25 +0100

    add card component for Isabelle B.

--- src/feature/cards.feature.tsx
+++ src/feature/cards.feature.tsx
@@ -6,6 +6,7 @@ const Cards: React.FC = () => {
         // <div className="flex flex-wrap justify-between gap-y-4">
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-x-8 gap-y-4">
             <Card firstName="Vincent B." characterId={8} />
+            <Card firstName="Isabelle B." characterId={4} />
         </div>
     )
 }
