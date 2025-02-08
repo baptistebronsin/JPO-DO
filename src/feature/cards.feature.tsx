a8b9d4bb2655bc70288acd67879c241dc92d4f90
Baptiste Bronsin <baptiste.bronsin@outlook.com>
2025-02-08 09:36:29 +0100

    add card

--- src/feature/cards.feature.tsx
+++ src/feature/cards.feature.tsx
@@ -7,6 +7,7 @@ const Cards: React.FC = () => {
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-x-8 gap-y-4">
             <Card firstName="Vincent B." characterId={8} />
             <Card firstName="Isabelle B." characterId={4} />
+            <Card firstName="Olivier DJ" characterId={53} />
         </div>
     )
 }
